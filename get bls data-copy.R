a02f28d9b59846dc715c15a6a9666e7ccf7e986b
Dann Hekman <dhekman@epic.com>
2016-12-24 11:08:37 -0600

    finished basic, rough draft of graphs.

--- get bls data-copy.R
+++ get bls data-copy.R
@@ -1,7 +1,7 @@
 ##Comparing Core inflation to medical inflation
 
 
-libs = c('blsAPI','zoo','ggplot2','data.table')
+libs = c('blsAPI','zoo','ggplot2','data.table','plyr')
 for (i in libs){
   if( !is.element(i, .packages(all.available = TRUE)) ) {
     install.packages(i)
@@ -22,19 +22,21 @@ for (i in libs){
 #Series 2: 'CUUR0000SAM' - Medical CPI
 
 
-rm(full.df)
-for(year in c(seq(from=1960, to=2010, by=10))) {
-     payload=list(
-          'seriesid'=c('CUUR0000SA0L1E','CUUR0000SAM'),
-          'startyear'=year,
-          'endyear'=year+9)
-     if(exists('full.df')) {
-          full.df <- rbind(full.df,blsAPI(payload,1,TRUE))
-     }
-     else {full.df <- blsAPI(payload,1,TRUE)}
+if (file.exists("fulldf.RDS")) {loadRDS("fulldf.RDS")}
+if (!exists('full.df')) {
+    for(year in c(seq(from=1960, to=2015, by=10))) {
+         payload=list(
+              'seriesid'=c('CUUR0000SA0L1E','CUUR0000SAM'),
+              'startyear'=year,
+              'endyear'=min(2015,year+9))
+         if(exists('full.df')) {
+              full.df <- rbind(full.df,blsAPI(payload,1,TRUE))
+         }
+         else {full.df <- blsAPI(payload,1,TRUE)}
+    }
+    saveRDS(full.df,"fulldf.RDS")
 }
 
-full.df <- blsAPI(payload, 1,TRUE) ;full.df
 full.df$value <- as.numeric(full.df$value)
 full.df$year  <- as.numeric(full.df$year)
 
@@ -54,3 +56,16 @@ ggplot(data=rbind(med.df,cor.df),aes(x=year,y=rate_increase,colour=seriesID)) +
       geom_line() +
       scale_color_brewer(palette="Set1")
 
+names(med.df) <- c("seriesID","year","med_value","med_rate_increase")
+names(cor.df) <- c("seriesID","year","cor_value","cor_rate_increase")
+byyear.df <- join(med.df,cor.df,by="year")
+byyear.df$med_cor_ratio <- byyear.df$med_rate_increase/byyear.df$cor_rate_increase
+
+hlines <- list('avg' = mean(byyear.df$med_cor_ratio),
+               'sdv' = sd(byyear.df$med_cor_ratio))
+hlines$hi <- hlines$avg + hlines$sdv
+hlines$lo <- hlines$avg - hlines$sdv
+ggplot(data=byyear.df, aes(x=year,y=med_cor_ratio)) +
+    geom_line() +
+    geom_hline(yintercept=hlines$lo,color="darkred",alpha=0.5) +
+    geom_hline(yintercept=hlines$hi,color="darkred",alpha=0.5) 
